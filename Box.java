2c295210155ecdb2812ad8a452c0339912d9f640
Shaun Tan <shauntanzongzhi@gmail.com>
2022-02-17 17:11:41 +0800

    Implement Box
    Passes Test1

--- Box.java
+++ Box.java
@@ -3,6 +3,35 @@
  * CS2030S Lab 4
  * AY21/22 Semester 2
  *
- * @author Put Your Name (Lab Group)
+ * @author Tan Zong Zhi, Shaun (Group 16A)
  */
 
+public class Box<T> {
+  private final T content;
+
+  private Box(T content) {
+    this.content = content;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Box<?>)) {
+      return false;
+    }
+
+    Box<?> boxObj = (Box<?>) obj;
+    return boxObj.content.equals(content);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[%s]", content);
+  }
+
+  public static <T> Box<T> of(T obj) {
+    if (obj == null) {
+      return null;
+    }
+    return new Box<T>(obj);
+  }
+}

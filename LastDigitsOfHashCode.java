8b45c19b4a34ad26ca2d0569d034aa8f27649b53
Shaun Tan <shauntanzongzhi@gmail.com>
2022-02-20 20:15:33 +0800

    Simplify LastDigitsOfHashCode

--- LastDigitsOfHashCode.java
+++ LastDigitsOfHashCode.java
@@ -18,11 +18,7 @@ public class LastDigitsOfHashCode implements Transformer<Object,
   @Override
   public Integer transform(Object input) {
     int hashCode = input.hashCode();
-    String hashStr = Integer.toString(hashCode);
-    int hashLen = hashStr.length();
-    String lastKChar = hashStr.substring(hashLen - k, hashLen);
-    String removeLeading0s = lastKChar.replaceAll("^0*", "");
-    String removeNegative = removeLeading0s.replaceAll("-", "");
-    return Integer.parseInt(removeNegative);
+    int positive = (hashCode < 0 ? -hashCode : hashCode);
+    return positive % (int) Math.pow(10, k);
   }
 }

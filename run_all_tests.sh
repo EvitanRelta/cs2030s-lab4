bbfadc5664141665c624191430c869274a8f6155
Shaun Tan <shauntanzongzhi@gmail.com>
2022-02-24 18:52:22 +0800

    Add 'run_all_tests.sh' to compile everything & run all 6 tests

---
+++ run_all_tests.sh
@@ -0,0 +1,7 @@
+javac -Xlint:rawtypes *.java
+java Test1 | grep failed;
+java Test2 | grep failed;
+java Test3 | grep failed;
+java Test4 | grep failed;
+java Test5 | grep failed;
+java Test6 | grep failed;
